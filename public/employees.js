0c2192e6e7ae2b5cff8042a8f8a8c7f8f2c562a7
Bob Painter <kuroppi@protonmail.com>
2023-05-07 15:20:04 -0700

    Initial Commit

---
+++ public/employees.js
@@ -0,0 +1,16 @@
+let zak = 'Zak Ruvalcaba';
+let sally = 'Sally Smith';
+let holly = 'Holly Unlikely';
+let amol = 'Amol Shookup';
+let robin = 'Robin Banks';
+let aaron = 'Arron D. Tires';
+let john = 'John Doe';
+let jane = 'Jane Doe';
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    'color': 'blue',
+    'fontSize': '24px'
+  }
+}, /*#__PURE__*/React.createElement("li", null, zak), /*#__PURE__*/React.createElement("li", null, sally), /*#__PURE__*/React.createElement("li", null, holly), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, robin), /*#__PURE__*/React.createElement("li", null, aaron), /*#__PURE__*/React.createElement("li", null, john), /*#__PURE__*/React.createElement("li", null, jane));
+ReactDOM.render(element, document.getElementById('content'));
+dental - assisting - cad - cam - combo - discount;
---
+++ src/employees.jsx
@@ -0,0 +1,26 @@
+let zak     = 'Zak Ruvalcaba'
+let sally   = 'Sally Smith'
+let holly   = 'Holly Unlikely'
+let amol    = 'Amol Shookup'
+let robin   = 'Robin Banks'
+let aaron   = 'Arron D. Tires'
+let john    = 'John Doe';
+let jane    = 'Jane Doe';
+
+const element = (
+    <ul style={{'color': 'blue', 'fontSize': '24px'}}>
+        <li>{zak}</li>
+        <li>{sally}</li>
+        <li>{holly}</li>
+        <li>{amol}</li>
+        <li>{robin}</li>
+        <li>{aaron}</li>
+        <li>{john}</li>
+        <li>{jane}</li>
+    </ul>
+)
+
+ReactDOM.render(element, document.getElementById('content'))
+
+
+dental-assisting-cad-cam-combo-discount
